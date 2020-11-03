f228ca918831e5e7409501856abbe507a2a8e2d6
Maximo100100 <Maximoabahamondes@gmail.com>
2020-11-02 16:48:39 -0800

    Integrated redux

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,27 +1,24 @@
-import React, { useEffect } from "react";
-import { loadStripe } from "@stripe/stripe-js";
+import React, { useEffect } from 'react';
+import CartItem from '../CartItem';
+import Auth from '../../utils/auth';
+import './style.css';
 import { useLazyQuery } from '@apollo/react-hooks';
-import { QUERY_CHECKOUT } from "../../utils/queries"
-import { idbPromise } from "../../utils/helpers"
-import CartItem from "../CartItem";
-import Auth from "../../utils/auth";
-import { useStoreContext } from "../../utils/GlobalState";
-import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
-import "./style.css";
+import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/store/actions';
+import { idbPromise } from "../../utils/helpers";
+import { QUERY_CHECKOUT } from '../../utils/queries';
+import { loadStripe } from '@stripe/stripe-js';
+
+// Import Redux
+import { useDispatch, useSelector } from 'react-redux'
 
 const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
+//Add Redux to Cart 
 const Cart = () => {
-  const [state, dispatch] = useStoreContext();
-  const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
+  const dispatch = useDispatch();
+  const state = useSelector(state => state);
 
-  useEffect(() => {
-    if (data) {
-      stripePromise.then((res) => {
-        res.redirectToCheckout({ sessionId: data.checkout.session })
-      })
-    }
-  }, [data]);
+  const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
 
   useEffect(() => {
     async function getCart() {
@@ -34,6 +31,14 @@ const Cart = () => {
     }
   }, [state.cart.length, dispatch]);
 
+  useEffect(() => {
+    if (data) {
+      stripePromise.then((res) => {
+        res.redirectToCheckout({ sessionId: data.checkout.session });
+      });
+    }
+  }, [data]);
+
   function toggleCart() {
     dispatch({ type: TOGGLE_CART });
   }
@@ -44,6 +49,16 @@ const Cart = () => {
       sum += item.price * item.purchaseQuantity;
     });
     return sum.toFixed(2);
+  }
+
+  if (!state.cartOpen) {
+    return (
+      <div className="cart-closed" onClick={toggleCart}>
+        <span
+          role="img"
+          aria-label="trash">🛒</span>
+      </div>
+    );
   }
 
   function submitCheckout() {
@@ -59,17 +74,6 @@ const Cart = () => {
       variables: { products: productIds }
     });
   }
-
-  if (!state.cartOpen) {
-    return (
-      <div className="cart-closed" onClick={toggleCart}>
-        <span
-          role="img"
-          aria-label="trash">🛒</span>
-      </div>
-    );
-  }
-
   return (
     <div className="cart">
       <div className="close" onClick={toggleCart}>[close]</div>
@@ -79,15 +83,13 @@ const Cart = () => {
           {state.cart.map(item => (
             <CartItem key={item._id} item={item} />
           ))}
-
           <div className="flex-row space-between">
             <strong>Total: ${calculateTotal()}</strong>
-
             {
               Auth.loggedIn() ?
                 <button onClick={submitCheckout}>
                   Checkout
-              </button>
+            </button>
                 :
                 <span>(log in to check out)</span>
             }
@@ -97,8 +99,8 @@ const Cart = () => {
           <h3>
             <span role="img" aria-label="shocked">
               😱
-          </span>
-          You haven't added anything to your cart yet!
+      </span>
+      You haven't added anything to your cart yet!
           </h3>
         )}
     </div>
